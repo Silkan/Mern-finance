e6e0ea6be0fa623db84180d4d828aa4e0f589927
Dylan McDonald <dylan.mcdonald@inhouselegalsolutions.com>
2023-12-15 16:07:03 +0000

    Add routing and components to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,11 @@
+import { Box } from "@mui/system";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { themeSettings } from "./theme";
+import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), [])
@@ -9,11 +13,19 @@ function App() {
   return (
     <>
       <div className='app'>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          hi 
-        </ThemeProvider>
-       </div>
+        <BrowserRouter>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+              <Navbar />
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/predictions" element={<div>Predictions page</div>} />
+              </Routes>
+            </Box>
+          </ThemeProvider>
+        </BrowserRouter>
+      </div>
     </>
   )
 }
